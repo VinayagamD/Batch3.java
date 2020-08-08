f3a15b62a3f2994115510bb5350713ca738b71a2
VinayagamD <vinayagam.d.ganesh@gmail.com>
2020-08-08 11:48:26 +0530

    Batch3 First Java Code

---
+++ Hello.java
@@ -0,0 +1,8 @@
+class Hello{
+    public static void main(String[] args){
+        System.out.println("Hello to java program");
+        for(String arg: args){
+            System.out.println(arg);
+        }
+    }
+}
